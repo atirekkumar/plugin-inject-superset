c89180408619f7993a525dc129ba343047da8b16
Atirek <atirek.kumar@couture.ai>
2021-08-31 13:10:17 +0530

    setup.sh working

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script will have the basic setup required by Superset
+# Make sure the docker daemon is running
+
+# variables
+WORKING_DIRECTORY=/Users/atirek/Documents/Couture/code # directory in which the script will run
+
+
+# enter working directory
+cd $WORKING_DIRECTORY
+
+# clone Apache Superset & switch to the version you want to use(in this case 1.3)
+git clone https://github.com/apache/superset
+cd $WORKING_DIRECTORY/superset
+git checkout 1.3
+
+# clone the superset-viz-plugins repo
+cd $WORKING_DIRECTORY
+git clone https://github.com/nielsen-oss/superset-viz-plugins
+
+# pull nielsen Superset 1.3 image
+docker pull nielsenoss/apache-superset:1.3
+
+# extract nielsen superset-frontend folder from image
+container=$(docker create nielsenoss/apache-superset:1.3)
+docker cp $container:/app/superset-frontend $WORKING_DIRECTORY
+docker rm -v $container
+
+# replace superset-frontend folder with the one from nielsen image
+rm -rf $WORKING_DIRECTORY/superset/superset-frontend
+mv $WORKING_DIRECTORY/superset-frontend $WORKING_DIRECTORY/superset/
