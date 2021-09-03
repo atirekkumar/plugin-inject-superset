32a8709152bfe50756e85b4a4d15d47a9378eff4
Atirek <atirek.kumar@couture.ai>
2021-09-03 19:04:53 +0530

    added to inject.sh

--- inject.sh
+++ inject.sh
@@ -20,42 +20,57 @@
 WORKING_DIRECTORY=/Users/atirek/Documents/Couture/code #directory in which the script will run
 NPM_ORG=improved-octo-succotash #name of the org used to publish package on npmjs
 PROJECT_WORKING_DIRECTORY=superset-viz-plugins #name of repo in which plugin is placed. Default it to 'superset-viz-plugins'
-PLUGIN_NAME=plugins-chart-custom-echarts #name of plugin being injected
-PRESET_NAME=NewPreset #name of preset file
-PLUGINS_EXTRA_FILENAME= #name of plugins extra file
+PLUGIN_NAME=plugin-chart-sankey-multilabel #name of plugin being injected
+PRESET_NAME=NewPreset2 #name of preset file
+PLUGINS_EXTRA_FILENAME=NewPreset2 #name of plugins extra file
 IMAGE_NAME= #name of docker image that will be generated
 
 # TODO: automatically get plugin code from npmjs
 
+# cd into superst-frontend
+cd "$WORKING_DIRECTORY"/superset/superset-frontend
 # add org to .npmrc
 echo @"$NPM_ORG":registry=http://registry.npmjs.org/ \
->> $WORKING_DIRECTORY/superset/superset-frontend/.npmrc
+>> "$WORKING_DIRECTORY"/superset/superset-frontend/.npmrc
 
 # add plugin as module to package.json
-GITHUB_WORKSPACE=$WORKING_DIRECTORY \
-PROJECT_WORKING_DIRECTORY=$PROJECT_WORKING_DIRECTORY \
-node ../../$PROJECT_WORKING_DIRECTORY/scripts/addDependencies.js  \
-$PLUGIN_NAME
+GITHUB_WORKSPACE="$WORKING_DIRECTORY" \
+PROJECT_WORKING_DIRECTORY="$PROJECT_WORKING_DIRECTORY" \
+node ../../"$PROJECT_WORKING_DIRECTORY"/scripts/addDependencies.js  \
+"$PLUGIN_NAME"
 
 # generate preset file
-GITHUB_WORKSPACE=$WORKING_DIRECTORY \
-PROJECT_WORKING_DIRECTORY=$PROJECT_WORKING_DIRECTORY \
-PRESET_NAME=$PRESET_NAME \
-node ../../$PROJECT_WORKING_DIRECTORY/scripts/generatePreset.js \
-$PLUGIN_NAME
+GITHUB_WORKSPACE="$WORKING_DIRECTORY" \
+PROJECT_WORKING_DIRECTORY="$PROJECT_WORKING_DIRECTORY" \
+PRESET_NAME="$PRESET_NAME" \
+node ../../"$PROJECT_WORKING_DIRECTORY"/scripts/generatePreset.js \
+"$PLUGIN_NAME"
 
 # move preset file to presets/ directory
 mv "$PRESET_NAME"Preset.ts src/visualizations/presets/"$PRESET_NAME"Preset.js
 
 # generate setuppluginsextra file
-GITHUB_WORKSPACE=$WORKING_DIRECTORY \
-PROJECT_WORKING_DIRECTORY=$PROJECT_WORKING_DIRECTORY \
-PRESET_NAME=$PRESET_NAME \
-PLUGINS_EXTRA_FILENAME=$PLUGINS_EXTRA_FILENAME \
-node ../../$PROJECT_WORKING_DIRECTORY/scripts/generateSetupPluginsExtra.js \
-$PLUGIN_NAME
+GITHUB_WORKSPACE="$WORKING_DIRECTORY" \
+PROJECT_WORKING_DIRECTORY="$PROJECT_WORKING_DIRECTORY" \
+PRESET_NAME="$PRESET_NAME" \
+PLUGINS_EXTRA_FILENAME="$PLUGINS_EXTRA_FILENAME" \
+node ../../"$PROJECT_WORKING_DIRECTORY"/scripts/generateSetupPluginsExtra.js \
+"$PLUGIN_NAME"
 
 # copy preset file to setup
-mv $PLUGINS_EXTRA_FILENAME "$PLUGINS_EXTRA_FILENAME".ts
-cp "$PLUGINS_EXTRA_FILENAME".ts src/setup/
+mv "$PLUGINS_EXTRA_FILENAME" "$PLUGINS_EXTRA_FILENAME".ts
+mv "$PLUGINS_EXTRA_FILENAME".ts src/setup/
 
+# replace deafult function name with file name
+# sed -i -e 's/setupPluginsExtra/"$PLUGINS_EXTRA_FILENAME"/' \
+# "$WORKING_DIRECTORY"/superset/superset-frontend/src/setup/"$PLUGINS_EXTRA_FILENAME".ts
+# 
+# call plugins_extra file in setupPlugins.ts
+# sed -i '\n' -e '/import MainPreset/a \
+# import "$PLUGINS_EXTRA_FILENAME" from "./"$PLUGINS_EXTRA_FILENAME"";' \
+# "$WORKING_DIRECTORY"/superset/superset-frontend/src/setup/setupPlugins.ts
+# 
+# 
+# sed -i '\n' -e '/setupPluginsExtra();/a \
+# "$PLUGINS_EXTRA_FILENAME"();'
+# "$WORKING_DIRECTORY"/superset/superset-frontend/src/setup/setupPlugins.ts
